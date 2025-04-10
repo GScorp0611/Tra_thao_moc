293943bbd3fcbc4cd1d8cb182d49d61c5fa8aab5
GScorp0611 <128770660+GScorp0611@users.noreply.github.com>
2025-04-10 21:17:26 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -18,3 +18,30 @@ function addProduct() {
 
   getProducts();
 }
+
+function getProducts() {
+  database.ref('products').on('value', function(snapshot) {
+    const data = snapshot.val();
+    const list = document.getElementById('productList');
+    list.innerHTML = '';
+
+    for (let id in data) {
+      const p = data[id];
+      list.innerHTML += `
+        <div class="product-item">
+          <h3>${p.name}</h3>
+          <img src="${p.image}" width="100">
+          <p>${p.price} VNĐ</p>
+          <button onclick="deleteProduct('${id}')">Xóa</button>
+        </div>
+      `;
+    }
+  });
+}
+
+function deleteProduct(id) {
+  database.ref('products/' + id).remove();
+  alert("Đã xóa sản phẩm!");
+}
+
+getProducts();
