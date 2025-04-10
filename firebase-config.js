57aea7819d834c27b85afa85875c1c68b97bb09f
GScorp0611 <128770660+GScorp0611@users.noreply.github.com>
2025-04-10 23:35:09 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,7 @@
 // Thay config của bạn vào đây
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD_jpUv4SaktjNOUVuMasdfgrq9mjRVgRQ",
   authDomain: "tra-thao-moc.firebaseapp.com",
@@ -9,5 +12,7 @@ const firebaseConfig = {
   appId: "1:899447552649:web:9d666dc3af63ac749ba458"
 };
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
