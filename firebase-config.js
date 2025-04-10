501da07ccadc59b4c985114e79f6f03bc0087707
GScorp0611 <128770660+GScorp0611@users.noreply.github.com>
2025-04-10 19:06:19 +0700

    Update and rename firebaseConfig.js to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,14 @@
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyD_jpUv4SaktjNOUVuMasdfgrq9mjRVgRQ",
   authDomain: "tra-thao-moc.firebaseapp.com",
   projectId: "tra-thao-moc",
-  storageBucket: "tra-thao-moc.firebasestorage.app",
+  storageBucket: "tra-thao-moc.appspot.com",
   messagingSenderId: "899447552649",
   appId: "1:899447552649:web:9d666dc3af63ac749ba458",
   measurementId: "G-E02LPD0L3Z"
 };
+
+// Khởi tạo firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
