6b74f1a2f77bd8f9e96119e0d1c403d45a19bcba
GScorp0611 <128770660+GScorp0611@users.noreply.github.com>
2025-04-10 19:01:04 +0700

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_jpUv4SaktjNOUVuMasdfgrq9mjRVgRQ",
+  authDomain: "tra-thao-moc.firebaseapp.com",
+  projectId: "tra-thao-moc",
+  storageBucket: "tra-thao-moc.firebasestorage.app",
+  messagingSenderId: "899447552649",
+  appId: "1:899447552649:web:9d666dc3af63ac749ba458",
+  measurementId: "G-E02LPD0L3Z"
+};
